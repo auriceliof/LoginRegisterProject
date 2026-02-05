16cdb6ed4109aa5c83b48cd7f37628912bc5b763
auriceliof <auricelio.suporte@hotmail.com>
2026-02-05 14:53:06 -0300

    add Login Page

--- frontend/src/components/InputField.jsx
+++ frontend/src/components/InputField.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-export function InputField({ label, type, autoComplete, name, error,register }) {
+export function InputField({
+  label,
+  type,
+  autoComplete,
+  name,
+  error,
+  register,
+}) {
   return (
     <div className="mb-4">
       <label htmlFor={name} className="block mb-1 font-medium">
@@ -12,7 +19,9 @@ export function InputField({ label, type, autoComplete, name, error,register }) 
         autoComplete={autoComplete}
         {...register(name)}
         className={`w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 ${
-          error ? "border-red-500 focus:ring-red-400" : "border-gray-300 focus:ring-blue-400"
+          error
+            ? "border-red-500 focus:ring-red-400"
+            : "border-gray-300 focus:ring-blue-400"
         }`}
       />
       {error && <p className="text-red-500 text-sm mt-1">{error.message}</p>}
