8c51432791b8904b9a29aa10b747ac8baead6906
auriceliof <auricelio.suporte@hotmail.com>
2026-02-05 15:56:43 -0300

    Add Register Page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -22,7 +22,7 @@ export function Login() {
           id="password"
           type="password"
           name="password"
-          autoComplete="email"
+          autoComplete="current-password"
           register={() => {}}
           error="Erro"
         />
